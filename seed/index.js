d781122c7649106e289658eb34987370ab16dfef
Kristine Lee <kristine0105@gmail.com>
2020-08-28 12:41:23 -0400

    user seed

--- seed/index.js
+++ seed/index.js
@@ -1,7 +1,9 @@
 const roomSeed = require("./roomSeed");
 const messageSeed = require("./messageSeed");
+const userSeed = require('./userSeed')
 
 module.exports = {
   roomSeed,
   messageSeed,
+  userSeed
 }
