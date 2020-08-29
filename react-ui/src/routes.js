1f6d7848a307a234782b5710118fc530f83ebb4a
Euny Jeon <ej1007@nyu.edu>
2020-08-29 15:53:10 -0400

    Under construction: working on routes

--- react-ui/src/routes.js
+++ react-ui/src/routes.js
@@ -1,29 +1,53 @@
 import React, { useEffect } from 'react'
 import { withRouter, Route, Switch } from 'react-router-dom'
 import { LoginPage, UserHome, Room } from './components'
-import { me } from './store'
-import { useSelector, useDispatch } from 'react-redux'
+
+// import { me } from './store'
+// import { useSelector, useDispatch } from 'react-redux'
+import { gql, useQuery } from '@apollo/client'
+
+const GET_ME = gql`
+  query getMe {
+    me {
+      id
+    }
+  }
+`
 
 /**
  * COMPONENT
  */
 
 const Routes = () => {
-  const isLoggedIn = useSelector((state) => !!state.user.id)
-  const dispatch = useDispatch()
-  const loadInitialData = () => {
-    dispatch(me())
-  }
+  // const isLoggedIn = useSelector((state) => !!state.user.id)
+  // const dispatch = useDispatch()
+  // const loadInitialData = () => {
+  //   dispatch(me())
+  // }
 
-  useEffect(() => {
-    loadInitialData()
-  })
+  const { loading, error, data } = useQuery(GET_ME)
+
+  console.log('GET_ME data: ', data)
+
+  if (loading)
+    return (
+      <>
+        <h1>Loading...</h1>
+      </>
+    )
+
+    if (error)
+    return (
+      <>
+        <h1>Error! ${error.message}</h1>
+      </>
+    )
 
   return (
     <Switch>
       {/* Routes placed here are available to all visitors */}
       <Route exact path="/" component={LoginPage} />
-      {isLoggedIn && (
+      {data.getMe.id && (
         <Switch>
           {/* Routes placed here are only available after logging in */}
           <Route path="/home" component={UserHome} />
@@ -37,4 +61,5 @@ const Routes = () => {
   )
 }
 
-export default withRouter(Routes)
+export default Routes
+// export default withRouter(Routes)
