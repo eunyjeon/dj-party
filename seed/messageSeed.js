c5c80e48fff346ac1ea3527625c3559d85891dd2
natalie_ng <natalie.ng.14755@gmail.com>
2020-08-31 16:43:53 -0400

    feat: room and subscription messages with seed works

--- seed/messageSeed.js
+++ seed/messageSeed.js
@@ -1,405 +1,405 @@
 
 
-const messageSeed = [{
-   "message": "Vivamus tortor.",
-  "userId": 5,
-  "roomId": 3
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 3,
-  "roomId": 5
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 8,
-  "roomId": 2
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 4,
-  "roomId": 4
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 3,
-  "roomId": 5
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 9,
-  "roomId": 2
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 4,
-  "roomId": 3
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 9,
-  "roomId": 5
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 3,
-  "roomId": 3
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Morbi ut odio.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Donec semper sapien a libero.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Proin risus.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Aenean fermentum.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Aliquam quis turpis eget elit sodales scelerisque.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Phasellus sit amet erat.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Aenean lectus.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Integer tincidunt ante vel ipsum.",
-  "userId": 7,
-  "roomId": 2
-}, {
-  "message": "In quis justo.",
-  "userId": 7,
-  "roomId": 1
-}, {
-  "message": "Etiam justo.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Integer tincidunt ante vel ipsum.",
-  "userId": 4,
-  "roomId": 4
-}, {
-  "message": "Quisque porta volutpat erat.",
-  "userId": 8,
-  "roomId": 5
-}, {
-  "message": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.",
-  "userId": 4,
-  "roomId": 1
-}, {
-  "message": "Donec vitae nisi.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Phasellus id sapien in sapien iaculis congue.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Aenean auctor gravida sem.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Pellentesque ultrices mattis odio.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Donec posuere metus vitae ipsum.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Sed sagittis.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "In hac habitasse platea dictumst.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Aenean fermentum.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Nam dui.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Suspendisse accumsan tortor quis turpis.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Curabitur gravida nisi at nibh.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Vivamus tortor.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Maecenas rhoncus aliquam lacus.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Donec posuere metus vitae ipsum.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Aenean sit amet justo.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Sed sagittis.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Nulla mollis molestie lorem.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Quisque ut erat.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Praesent blandit.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Integer tincidunt ante vel ipsum.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Sed sagittis.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Nulla tempus.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "In blandit ultrices enim.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Aliquam sit amet diam in magna bibendum imperdiet.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Nulla tempus.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Aenean lectus.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Suspendisse potenti.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Nullam porttitor lacus at turpis.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Sed sagittis.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "In hac habitasse platea dictumst.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Phasellus in felis.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Nulla ut erat id mauris vulputate elementum.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Morbi porttitor lorem id ligula.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Integer a nibh.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Praesent id massa id nisl venenatis lacinia.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Integer ac leo.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Donec dapibus.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Vestibulum rutrum rutrum neque.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Sed ante.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Suspendisse ornare consequat lectus.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Cras pellentesque volutpat dui.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Suspendisse ornare consequat lectus.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Nulla suscipit ligula in lacus.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Integer a nibh.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "Sed ante.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Duis aliquam convallis nunc.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Fusce posuere felis sed lacus.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "In quis justo.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Morbi ut odio.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "In hac habitasse platea dictumst.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Aenean sit amet justo.",
-  "userId": 1,
-  "roomId": 1
-}, {
-  "message": "In blandit ultrices enim.",
-  "userId": 1,
-  "roomId": 4
-}, {
-  "message": "Maecenas pulvinar lobortis est.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Phasellus id sapien in sapien iaculis congue.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Morbi vel lectus in quam fringilla rhoncus.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
-  "userId": 1,
-  "roomId": 2
-}, {
-  "message": "Nullam molestie nibh in lectus.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Nulla mollis molestie lorem.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Vestibulum sed magna at nunc commodo placerat.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Integer non velit.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Pellentesque at nulla.",
-  "userId": 1,
-  "roomId": 3
-}, {
-  "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
-  "userId": 1,
-  "roomId": 5
-}, {
-  "message": "Morbi ut odio.",
-  "userId": 1,
-  "roomId": 3
-}]
+// const messageSeed = [{
+//    "message": "Vivamus tortor.",
+//   "userId": 5,
+//   "roomId": 3
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 3,
+//   "roomId": 5
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 8,
+//   "roomId": 2
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 4,
+//   "roomId": 4
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 3,
+//   "roomId": 5
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 9,
+//   "roomId": 2
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 4,
+//   "roomId": 3
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 9,
+//   "roomId": 5
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 3,
+//   "roomId": 3
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Morbi ut odio.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Donec semper sapien a libero.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Proin risus.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Aenean fermentum.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Aliquam quis turpis eget elit sodales scelerisque.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Phasellus sit amet erat.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Aenean lectus.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Integer tincidunt ante vel ipsum.",
+//   "userId": 7,
+//   "roomId": 2
+// }, {
+//   "message": "In quis justo.",
+//   "userId": 7,
+//   "roomId": 1
+// }, {
+//   "message": "Etiam justo.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Integer tincidunt ante vel ipsum.",
+//   "userId": 4,
+//   "roomId": 4
+// }, {
+//   "message": "Quisque porta volutpat erat.",
+//   "userId": 8,
+//   "roomId": 5
+// }, {
+//   "message": "Integer aliquet, massa id lobortis convallis, tortor risus dapibus augue, vel accumsan tellus nisi eu orci.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.",
+//   "userId": 4,
+//   "roomId": 1
+// }, {
+//   "message": "Donec vitae nisi.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Phasellus id sapien in sapien iaculis congue.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Aenean auctor gravida sem.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Morbi sem mauris, laoreet ut, rhoncus aliquet, pulvinar sed, nisl.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Pellentesque ultrices mattis odio.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Donec odio justo, sollicitudin ut, suscipit a, feugiat et, eros.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Donec posuere metus vitae ipsum.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Sed sagittis.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "In hac habitasse platea dictumst.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Aenean fermentum.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Nam dui.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Suspendisse accumsan tortor quis turpis.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Curabitur gravida nisi at nibh.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Vivamus tortor.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Maecenas rhoncus aliquam lacus.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Donec posuere metus vitae ipsum.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Aenean sit amet justo.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Sed sagittis.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Nulla mollis molestie lorem.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Quisque ut erat.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Praesent blandit.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Integer tincidunt ante vel ipsum.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Sed sagittis.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Nulla tempus.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "In blandit ultrices enim.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Aliquam sit amet diam in magna bibendum imperdiet.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Nulla tempus.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Aenean lectus.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Suspendisse potenti.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Nullam porttitor lacus at turpis.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Sed sagittis.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "In hac habitasse platea dictumst.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Phasellus in felis.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Nulla ut erat id mauris vulputate elementum.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Morbi porttitor lorem id ligula.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Integer a nibh.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Praesent id massa id nisl venenatis lacinia.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Integer ac leo.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Donec dapibus.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Vestibulum rutrum rutrum neque.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Sed ante.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Suspendisse ornare consequat lectus.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Cras pellentesque volutpat dui.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Suspendisse ornare consequat lectus.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Nulla suscipit ligula in lacus.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Integer a nibh.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "Sed ante.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Duis aliquam convallis nunc.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Fusce posuere felis sed lacus.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "In quis justo.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Morbi vestibulum, velit id pretium iaculis, diam erat fermentum justo, nec condimentum neque sapien placerat ante.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Morbi ut odio.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "In hac habitasse platea dictumst.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Aenean sit amet justo.",
+//   "userId": 1,
+//   "roomId": 1
+// }, {
+//   "message": "In blandit ultrices enim.",
+//   "userId": 1,
+//   "roomId": 4
+// }, {
+//   "message": "Maecenas pulvinar lobortis est.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Phasellus id sapien in sapien iaculis congue.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Morbi vel lectus in quam fringilla rhoncus.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Vestibulum quam sapien, varius ut, blandit non, interdum in, ante.",
+//   "userId": 1,
+//   "roomId": 2
+// }, {
+//   "message": "Nullam molestie nibh in lectus.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Nulla mollis molestie lorem.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Vestibulum sed magna at nunc commodo placerat.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Integer non velit.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Pellentesque at nulla.",
+//   "userId": 1,
+//   "roomId": 3
+// }, {
+//   "message": "Lorem ipsum dolor sit amet, consectetuer adipiscing elit.",
+//   "userId": 1,
+//   "roomId": 5
+// }, {
+//   "message": "Morbi ut odio.",
+//   "userId": 1,
+//   "roomId": 3
+// }]
 
-module.exports = messageSeed
+// module.exports = messageSeed
