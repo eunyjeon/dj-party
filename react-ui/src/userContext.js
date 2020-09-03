8de0bd6a708ea98ab51a073397f4f0b2b1225724
natalie_ng <natalie.ng.14755@gmail.com>
2020-09-03 19:42:13 -0400

    fix: rendering, but context is null

--- react-ui/src/userContext.js
+++ react-ui/src/userContext.js
@@ -1,18 +1,18 @@
-// import React from "react";
-// import { useQuery } from '@apollo/client'
-// import { GET_ME } from './graphql'
+import React from "react";
+import { useQuery } from '@apollo/client'
+import { GET_ME } from './graphql'
 
-// const UserContext = React.createContext({});
+const UserContext = React.createContext({});
 
-// export const UserProvider = props => {
-//   const { loading, data } = useQuery(GET_ME)
-//   const user = loading || !data.gotUser ? null: data.user
-//   return (
-//     <UserContext.Provider value={user}>
-//       {props.children}
-//     </UserContext.Provider>
-//   )
-// }
+export const UserProvider = props => {
+  const { loading, data } = useQuery(GET_ME)
+  const user = loading || !data.gotUser ? null: data.user
+  return (
+    <UserContext.Provider value={user}>
+      {props.children}
+    </UserContext.Provider>
+  )
+}
 
-// export const UserConsumer = UserContext.Consumer;
-// export default UserContext;
+export const UserConsumer = UserContext.Consumer;
+export default UserContext;
