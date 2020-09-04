5efc436d1826d69eb0390dd93a0b0f714098f97e
natalie_ng <natalie.ng.14755@gmail.com>
2020-09-03 23:15:48 -0400

    feat: setting up player

--- react-ui/src/components/singleRoom/legacy.js
+++ react-ui/src/components/singleRoom/legacy.js
@@ -327,3 +327,21 @@
 
 // //   return <StyledPlayer></StyledPlayer>
 // // }
+
+
+//  import React, { Component } from "react";
+
+
+// export default class Player extends Component {
+// 	public constructor(props: ISpotifyPlayerProps) {
+// 		super(props);
+	
+// 		new ScriptCache([
+// 			{
+// 				name: "https://sdk.scdn.co/spotify-player.js",
+// 				callback: this.spotifySDKCallback
+// 			}]);
+	
+// 	   }])
+// 	// ...
+// 	}
