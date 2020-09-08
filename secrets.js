f83af05587624e985f490b152366a72c7cae31f9
Deanna Dunne <deanna.dunne@gmail.com>
2020-09-08 10:04:29 -0400

    heroku attempt

--- secrets.js
+++ secrets.js
@@ -2,3 +2,5 @@ process.env.CLIENT_ID = '0c88d555ef3c4410bf6f4cb02793199f'
 process.env.CLIENT_SECRET = '4ee19343e83e496aa1723ba7008fbe7f'
 
 process.env.ROOT_URL = 'http://localhost:5000'
+
+process.env.SERVER_URL = 'http://localhost:4000'
