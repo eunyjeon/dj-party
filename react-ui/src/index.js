b24e2b5f7e5bee50d8c3221fc7c9c0fd50618887
Deanna Dunne <deanna.dunne@gmail.com>
2020-09-08 11:56:28 -0400

    Heroku attempt

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -14,14 +14,16 @@ import { HttpLink } from 'apollo-link-http'
 import { SubscriptionClient } from 'subscriptions-transport-ws'
 import './index.css'
 
+let PORT = process.env.PORT || 4000
+
 // for queries & mutations
 const httpLink = new HttpLink({
   // uri: 'http://localhost:4000',
-  uri: 'http://localhost:4000/graphql',
+  uri: `http://0.0.0.0:${PORT}/graphql`,
 })
 
 // for subscription & push notifications
-const GRAPHQL_ENDPOINT = `ws://localhost:4000/graphql`
+const GRAPHQL_ENDPOINT = `ws://0.0.0.0:${PORT}/graphql`
 const clientWS = new SubscriptionClient(GRAPHQL_ENDPOINT, {
   reconnect: true,
 })
