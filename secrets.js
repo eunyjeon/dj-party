2b1beee9aea7600a1c50564adad26a579fbeb5a0
Deanna Dunne <deanna.dunne@gmail.com>
2020-09-08 10:22:56 -0400

    Heroku attempt

--- secrets.js
+++ secrets.js
@@ -3,4 +3,6 @@ process.env.CLIENT_SECRET = '4ee19343e83e496aa1723ba7008fbe7f'
 
 process.env.ROOT_URL = 'http://localhost:5000'
 
-process.env.SERVER_URL = 'http://localhost:4000'
+process.env.SERVER_URL = 'localhost:4000'
+
+process.env.APOLLO_KEY = 'service:djParty:_nioegrp5NJBn-3mywtHhQ'
